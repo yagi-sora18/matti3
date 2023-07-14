5e9fdcfa1c28cd4b0654cef8084f5d062f725a66
yagi-sora18 <138739113+yagi-sora18@users.noreply.github.com>
2023-07-14 12:09:31 +0900

    ソースとヘッダーに追加
    ソースとヘッダーに追加しました。２

---
+++ match3(2)/match3(2)/header/InputControl.h
@@ -0,0 +1,40 @@
+#pragma once
+
+/**********************************
+
+*マクロ定義
+
+***********************************/
+
+
+/**********************************
+
+*型定義
+
+***********************************/
+
+
+/**********************************
+
+*プロトタイプ宣言
+
+***********************************/
+
+void Input_Initialize(void);
+void Input_Update(void);
+int Input_Escape(void);
+
+
+
+int GetOldKey(int key);
+int GetNowKey(int key);
+int GetKeyFlg(int key);
+int GetMousePositionX(void);
+int GetMousePositionY(void);
+
+
+
+
+
+
+
--- match3(2)/match3(2)/sourcecode/Stage.cpp
+++ match3(2)/match3(2)/sourcecode/Stage.cpp
@@ -87,3 +87,130 @@ void combo_check_w(int y, int x, int* cnt, int* col);
 void save_Block(void);
 void restore_block(void);
 
+
+/******************************************
+
+*ステージ制御機能：初期化処理
+
+*引数：なし
+ 
+*戻り値：エラー情報
+
+*******************************************/
+
+int StageInitialize(void)
+{
+	int ret = 0;
+	int i;
+
+	//画像読み込み
+	LoadDivGraph("images/block.png", BLOCK_IMAGE_MAX, BLOCK_IMAGE_MAX, 1, BLOCKSIZE, BLOCKSIZE, BlockImage);
+
+
+	//音源読み込み
+	ClickSE = LoadSoundMem("sounds/click_se.mp3");
+	FadeOutSE = LoadSoundMem("sounds/fadeout_se.mp3");
+	MoveBlockSE = LoadSoundMem("sounds/moveblock_se.mp3");
+
+	//ブロック生成処理
+	CreateBlock();
+
+
+	ClickStatus = E_NONE;
+	Stage_State = 0;
+	Stage_Score = 0;
+	ClearFlag = FALSE;
+
+
+	for (i = 0; i < 3; i++)
+	{
+		Select[i].x = 0;
+		Select[i].y = 0;
+	}
+
+	//エラーチェック
+	for (i = 0; i < BLOCK_IMAGE_MAX; i++)
+	{
+		if (BlockImage[i] == -1)
+		{
+			ret = -1;
+			break;
+		}
+	}
+
+	if (StageImage == -1)
+	{
+		ret = -1;
+	}
+	if (FadeOutSE == -1)
+	{
+		ret = -1;
+	}
+	if (MoveBlockSE == -1)
+	{
+		ret = -1;
+	}
+
+
+	return ret;
+}
+
+/*********************************************
+
+*ステージ制御機能：ステージの描画
+
+*引数：なし
+
+*戻り値：なし
+
+**********************************************/
+
+void StageDraw(void){
+	DrawGraph(0, 0, StageImage, FALSE);
+
+	//アイテムの取得個数を描画
+	for (int i = 0; i < ITEM_MAX; i++)
+	{
+		DrawRotaGraph(540, 245 + i * 30, 0.5f, 0, BlockImage[i + 1], TRUE, 0);
+		DrawFormatString(580, 235 + i * 30, 0xffffff, "%3d", Item[i]);
+	}
+	//ブロックを描画
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (Block[i][j].flg == TRUE && Block[i][j].image != NULL)
+			{
+				DrawGraph
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
